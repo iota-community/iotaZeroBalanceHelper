219bcafa2252140e9d8a6ac2fa6a06bf006476ce
Antonio Nardella <antonio.nardella@iota.org>
2022-01-06 14:35:30 +0100

    added curlp address export

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/HBMY289/iotaZeroBalanceHelper/reclaim"
-	"github.com/HBMY289/iotaZeroBalanceHelper/userIO"
-	"github.com/HBMY289/iotaZeroBalanceHelper/balance"
-	"github.com/HBMY289/iotaZeroBalanceHelper/address"
+	"github.com/iota-community/iotaZeroBalanceHelper/reclaim"
+	"github.com/iota-community/iotaZeroBalanceHelper/userIO"
+	"github.com/iota-community/iotaZeroBalanceHelper/balance"
+	"github.com/iota-community/iotaZeroBalanceHelper/address"
 )
 
 
@@ -35,16 +35,18 @@ func mainMenu(seed string) {
 		opt := userIO.GetOption("Choose from available options:", []string{"check balance", "check for possible reclaim", "Exit"})
 		switch opt {
 			case 1:
-				checkBalance(seed)
-			case 2: 
+				checkKerlBalance(seed)
+			case 2:
+				checkCurlPBalance(seed)
+			case 3: 
 				checkReclaim(seed)	
-			case 3:
+			case 4:
 				os.Exit(0)
 		}		
 	}		
 }
 
-func checkBalance(seed string) {
+func checkKerlBalance(seed string) {
 	balance.LoadSnapshot()
 	addrCount := userIO.GetNumberOfAddresses ()
 	fmt.Println("\nGenerating addresses")
@@ -66,6 +68,28 @@ func checkBalance(seed string) {
 	userIO.WaitforEnter()
 }
 
+
+func checkCurlPBalance(seed string) {
+	balance.LoadSnapshot()
+	addrCount := userIO.GetNumberOfAddresses ()
+	fmt.Println("\nGenerating addresses")
+	addrs := address.GetCurlPddresses(seed,addrCount)
+	total, bals := balance.GetBalance(addrs)
+	if total == 0 {
+		fmt.Printf("\nNo funds were found on the first %d addresses. Either the funds have been moved or you entered the wrong seed.\n", addrCount)
+
+		
+	} else{
+		fmt.Printf("\nA total balance of %d was found on the first %d addresses.\n", total, addrCount)
+		for addr, bal := range bals {
+			fmt.Printf("balance: %d on address: %s\n",bal,addr)
+		}
+	}
+	if userIO.GetConfirmation ("Do you want to export the generated addresses for reference?") {
+		userIO.ExportAddresses(addrs)
+	}
+	userIO.WaitforEnter()
+}
 
 
 func checkReclaim(seed string) {
